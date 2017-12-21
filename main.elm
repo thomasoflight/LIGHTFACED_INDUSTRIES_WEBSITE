081108dcb9e5a943659a644078c6adfa52c393b8
A.M. Thomas <thomasoflight@lightfaced.org>
2017-12-21 18:16:00 -0500

    Initial commit

---
+++ main.elm
@@ -0,0 +1,19 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+
+
+view =
+    div [ class "container" ]
+        [ h1 [] [ text "lumo" ]
+        ]
+
+
+
+-- update : Msg -> Model
+
+
+main =
+    view
